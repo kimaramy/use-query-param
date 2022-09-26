7e8ef041d49d1db2cdd9fee476a4c21614b36d8c
kimaramy <haram@koreaspacedata.com>
2022-09-26 20:40:09 +0900

    add example dir

---
+++ example/src/app/App.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  useQueryParams,
+  useQueryParam,
+} from '@_kimaramy_/use-query-params/src';
+import './App.css';
+
+const App: React.FC = () => {
+  const [queryParams, setQueryParams] = useQueryParams<'a' | 'b'>({
+    isShallow: true,
+  });
+  const [a, setA] = useQueryParam('a', (value) => Number(value), {
+    isShallow: false,
+  });
+
+  console.log('app mounted');
+  console.log('queryParams', queryParams);
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <dl>
+        <dt>query params</dt>
+        <dd>
+          <code>{JSON.stringify(queryParams)}</code>
+        </dd>
+        <dt>query param a</dt>
+        <dd>
+          <code>{JSON.stringify({ a })}</code>
+        </dd>
+        <dt>history.length</dt>
+        <dd>{window.history.length}</dd>
+        <dt>location.pathname</dt>
+        <dd>{window.location.pathname}</dd>
+      </dl>
+
+      <button
+        onClick={() => {
+          setQueryParams({});
+        }}
+      >
+        clear
+      </button>
+
+      <button
+        onClick={() => {
+          setQueryParams({ a: 1 });
+        }}
+      >
+        reset with a
+      </button>
+
+      <button
+        onClick={() => {
+          setQueryParams({ ...queryParams, a: 2 });
+        }}
+      >
+        upsert a
+      </button>
+
+      <button
+        onClick={() => {
+          setQueryParams({ ...queryParams, b: 2 });
+        }}
+      >
+        upsert b
+      </button>
+
+      <button
+        onClick={() => {
+          setQueryParams({ ...queryParams, b: undefined });
+        }}
+      >
+        delete b
+      </button>
+
+      <button
+        onClick={() => {
+          setA(2);
+        }}
+      >
+        upsert a
+      </button>
+    </div>
+  );
+};
+
+export default App;
