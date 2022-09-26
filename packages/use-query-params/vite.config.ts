287d191b240751d07556cde6ecc801e73ffcaa8c
kimaramy <haram@koreaspacedata.com>
2022-09-26 19:23:54 +0900

    update package essentials

--- packages/use-query-params/vite.config.ts
+++ packages/use-query-params/vite.config.ts
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dts()],
   test: {},
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'useQueryParams',
+      fileName: 'use-query-params',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
 });
